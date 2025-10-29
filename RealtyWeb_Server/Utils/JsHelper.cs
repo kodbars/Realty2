f6558d97a4626ca492a5e2099f0b82ff57dce97b
Andrey <andrey.barsukov.bp@gmail.com>
2025-10-29 17:25:08 +0300

    4 commit

---
+++ RealtyWeb_Server/Utils/JsHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace RealtyWeb_Server.Utils
+{
+    public static class JsHelper
+    {
+        public static async ValueTask NotyfSuccess(this IJSRuntime js, string alertText) 
+        {
+            await js.InvokeVoidAsync("DisplayNotyf", "success", alertText);
+        }
+        public static async ValueTask NotyfError(this IJSRuntime js, string alertText)
+        {
+            await js.InvokeVoidAsync("DisplayNotyf", "error", alertText);
+        }
+    }
+}
