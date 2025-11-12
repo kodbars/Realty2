2c44e81914abb4b66bccc716e18f720b00c8c96a
Victoria <theviktori39@gmail.com>
2025-11-12 05:23:38 +0300

    9 commit

---
+++ RealtyWeb_Server/Utils/StateContainerService.cs
@@ -0,0 +1,14 @@
+﻿namespace RealtyWeb_Server.Utils
+{
+    public class StateContainerService
+    {
+        public int Value { get; set; } = 0;
+        public event Action OnStateChange = null!;
+        public void SetValue(int value) 
+        {
+            Value = value;
+            NotifyStateChanged();
+        }
+        private void NotifyStateChanged() => OnStateChange?.Invoke();
+    }
+}
