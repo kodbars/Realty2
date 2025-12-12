9e74056f8b5768970b35ca75747fb99085a0936a
Andrey <andrey.barsukov.bp@gmail.com>
2025-12-12 16:28:58 +0300

    18 commit

--- Realty_Models/Region.cs
+++ Realty_Models/Region.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,10 @@ namespace Realty_Models
     public class Region
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Введите наименование")]
         public string? Nm { get; set; }
+        [Required(ErrorMessage = "Введите код")]
+        [RegularExpression(@"\d{2}", ErrorMessage = "Некорректный код")]
         public string? GIBDD { get; set;}
     }
 }
